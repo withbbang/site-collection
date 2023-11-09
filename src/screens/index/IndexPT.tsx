5d903dc5ddfded70cf32b3a3e9358ab166c1a54c
김영선 <withbbang@gmail.com>
2023-11-09 18:05:06 +0900

    Test: confirm popup setting 모듈 테스트 원복

--- src/screens/index/IndexPT.tsx
+++ src/screens/index/IndexPT.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import styles from './Index.module.scss';
 
-function IndexPT({ onClick }: typeIndexPT): React.JSX.Element {
+function IndexPT({}: typeIndexPT): React.JSX.Element {
   return (
     <div className={styles.wrap}>
       <h1>Index Page</h1>
-      <button onClick={onClick}>Add</button>
     </div>
   );
 }
 
-interface typeIndexPT {
-  onClick: () => void;
-}
+interface typeIndexPT {}
 
 export default IndexPT;
