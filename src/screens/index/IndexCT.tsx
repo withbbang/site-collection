18f62ac4c8f94e97410e58bc2f8c5a4838457cdc
김영선 <withbbang@gmail.com>
2023-11-09 15:10:09 +0900

    Test: add, update 커스텀 훅 테스트

--- src/screens/index/IndexCT.tsx
+++ src/screens/index/IndexCT.tsx
@@ -1,17 +1,32 @@
 import React, { useEffect } from 'react';
 import { CommonState } from 'middlewares/reduxToolkits/commonSlice';
-import { handleGetDocuments } from 'modules/customHooks';
+import {
+  useAddDocumentHook,
+  useGetDocumentsHook,
+  useUpdateDocumentHook,
+} from 'modules/customHooks';
 import IndexPT from './IndexPT';
 
 function IndexCT({}: typeIndexCT): React.JSX.Element {
-  const docs = handleGetDocuments('Links');
+  const docs = useGetDocumentsHook('Links');
+  const useAddDocument = useAddDocumentHook(
+    'Links',
+    { link: 'www.google.com' },
+    () => console.log('successCb'),
+  );
+  const useUpdateDocument = useUpdateDocumentHook(
+    'Links',
+    '3VaxoKEqKAd67NkXUNFb',
+    { link: 'https://www.google.com' },
+    () => console.log('successCb'),
+  );
 
   // 로그인 여부 판단 훅
   useEffect(() => {
     console.log(docs);
   }, [docs]);
 
-  return <IndexPT />;
+  return <IndexPT onClick={useAddDocument} onupdate={useUpdateDocument} />;
 }
 
 interface typeIndexCT extends CommonState {}
--- src/screens/index/IndexPT.tsx
+++ src/screens/index/IndexPT.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import styles from './Index.module.scss';
 
-function IndexPT({}: typeIndexPT): React.JSX.Element {
+function IndexPT({ onClick, onupdate }: typeIndexPT): React.JSX.Element {
   return (
     <div className={styles.wrap}>
       <h1>Index Page</h1>
+      <button onClick={onClick}>useAddDocument test</button>
+      <button onClick={onupdate}>useUpdateDocument test</button>
     </div>
   );
 }
 
-interface typeIndexPT {}
+interface typeIndexPT {
+  onClick: () => void;
+  onupdate: () => void;
+}
 
 export default IndexPT;
